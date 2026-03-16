bfb1bb006d471a597445fccd9bfd1b1a33274b24
strats11 <hstratford_10@yahoo.co.uk>
2026-03-16 10:08:03 +0000

    Add routing with BrowserRouter and Dashboard route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Dashboard from './Dashboard.jsx'
 import './index.css'  // <--- This line is critical!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
